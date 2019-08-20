8aaaaa321e1fff54a8d91c7a3b97d5ff94691311
todd.ma <todd.ma@qunar.com>
2019-08-20 16:57:23 +0800

    pmx 启动模式下，直接require messenger 模块

--- src/index.js
+++ src/index.js
@@ -1,7 +1,6 @@
-
-const { fork } = require('child_process')
-const assert = require('assert')
-const { instrument } = require('./lib/instrument')
+const {fork} = require('child_process');
+const assert = require('assert');
+const {instrument} = require('./lib/instrument');
 const path = require('path')
 
 /**
@@ -9,44 +8,34 @@ const path = require('path')
  * @param opt {rootDir, startFile} rootDir:服务根目录, startFile:启动文件
  */
 exports.qncServerStart = async (opt) => {
-  const { rootDir, startFile } = opt
-  assert(rootDir && startFile, 'rootDir or startFile nonempty')
+    const {rootDir, startFile} = opt;
+    assert(rootDir && startFile, 'rootDir or startFile nonempty');
 
-  let err
+    let err;
 
-  try {
-    err = await instrument(opt.rootDir)
+    try {
+        err = await instrument(opt.rootDir);
 
-    // 启动用户指定的服务并携带信使
-    const userProcess = fork(opt.startFile, {
-      execArgv: ['--require', `${path.resolve(__dirname, './messenger.js')}`]
-    })
+        //启动用户指定的服务并携带信使
+        const userProcess = fork(opt.startFile, {
+            execArgv: ['--require', `${path.resolve(__dirname, './messenger.js')}`]
+        });
 
-    // 启动覆盖率分析服务
-    const reportProcess = fork(path.join(__dirname, './lib/report.js'))
+        //启动覆盖率分析服务
+        const reportProcess = fork(path.join(__dirname, './lib/report.js') );
 
-    // 获取用户服务进程内的覆盖率数据，发送给覆盖率服务进程
-    userProcess.on('message', (m) => {
-      reportProcess.send(m)
-    })
-  } catch (error) {
-    console.log(error)
-  }
+        //获取用户服务进程内的覆盖率数据，发送给覆盖率服务进程
+        userProcess.on('message', (m) => {
+            reportProcess.send(m);
+        });
+
+    } catch (error) {
+        console.log(error);
+    }
 }
 
 
-
-// setInterval(() => {
-//   process.send({
-//     type : 'process:msg',
-//     data : {
-//      success : true
-//     }
-//  });
-
-//  console.log(+new Date)
-// }, 1000)
-
-console.log(process.env)
-
-process.exit()
+// pm2 通过 node -r=qnc 的方式启动
+if(process.env.pmx){
+  require('./messenger')
+}
