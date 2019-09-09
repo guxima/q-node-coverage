af89906dd9a2b6ac1dfd41fe8c756dceab1c7507
todd.ma <todd.ma@qunar.com>
2019-09-09 14:23:31 +0800

    feat: 下载代码覆盖率

--- src/lib/report.js
+++ src/lib/report.js
@@ -2,15 +2,27 @@
  * 启动服务对外提供覆盖率数据，
  * 在请求详细覆盖率统计数据的时候，生成覆盖率报告HTML
  */
+const os = require('os')
 const path = require('path')
+const fs = require('fs')
 const express = require('express')
 const libCoverage = require('istanbul-lib-coverage')
-const { createReporter } = require('istanbul-api')
+const { createReporter, config } = require('istanbul-api')
 const debug = require('debug')('qnc:report')
+const archiver = require('archiver')
 
+const QncTmpDir = path.resolve(os.tmpdir(), 'qnc')
 const { ProcessMessageType, CoverageServerPort } = require('./constant')
 
+const istanbulConfig = config.loadObject({
+  verbose: true,
+  reporting: {
+    dir: path.join(QncTmpDir, 'coverage') // 报告输出的目录
+  }
+})
+
 let CoverageMap = null // 默认的覆盖率数据，用户服务的数据会通过IPC的形式传进来
+const ZipFile = 'coverage.zip'
 
 process.on('message', ({ type, data }) => {
   // 只处理qnc发出的进程消息
@@ -24,11 +36,6 @@ process.on('message', ({ type, data }) => {
       CoverageMap.merge(coverage)
       debug('merge coverage')
     }
-
-    const reporter = createReporter()
-
-    reporter.addAll(['html'])
-    reporter.write(CoverageMap)
   }
 })
 
@@ -45,9 +52,7 @@ process.on('uncaughtException', function (err) {
 
 const app = express()
 
-const projectDir = process.cwd()
-
-app.use(express.static(path.join(projectDir, 'coverage')))
+// app.use(express.static(path.join(projectDir, 'coverage')))
 
 app.get('/coverage', (req, res) => {
   const { file } = req.query
@@ -55,6 +60,32 @@ app.get('/coverage', (req, res) => {
     return new RegExp(`${file}$`).test(k)
   })
   res.send(CoverageMap[key] || CoverageMap)
+})
+
+app.get('/report-package', (req, res) => {
+  let reporter = createReporter(istanbulConfig)
+
+  reporter.add('html')
+  reporter.write(CoverageMap)
+
+  reporter = null
+
+  const archive = archiver('zip', { zlib: { level: 9 } })
+
+  archive.on('error', err => {
+    console.error('zip file got error: %O', err)
+    res.send(err)
+  })
+
+  const output = fs.createWriteStream(path.join(QncTmpDir, ZipFile))
+
+  output.on('close', () => {
+    res.download(path.join(QncTmpDir, ZipFile))
+    debug(`${ZipFile} downloaded ${archive.pointer()} total bytes`)
+  })
+  archive.pipe(output)
+  archive.directory(path.join(QncTmpDir, 'coverage'), 'coverage')
+  archive.finalize()
 })
 
 app.get('/summary', (req, res) => {
